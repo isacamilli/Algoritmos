1fd59fbf4f162830e4c2b95f87235474d5db3019
isacamilli <165300352+isacamilli@users.noreply.github.com>
2024-11-07 17:27:49 +0000

    jogo

--- campo_minado/desafio.c
+++ campo_minado/desafio.c
@@ -24,36 +24,63 @@ int main() {
         for (int y = 0; y < 5; y++) {
             int contador = 0;
             if (campo[x][y] == 0){
-                if (x < 4 || y < 4){
-                    if (campo[x][y+1]==-1) {
-                        contador++;
-                    }
-                    if (campo[x+1][y]==-1) {
-                        contador++;
-                    }
-                    if (campo[x+1][y+1]==-1) {
-                        contador++;
-                    }
+                if (campo[x][y+1]==-1) {
+                    contador++;
                 }
-                else {
-                    if(campo[x-1][y]==-1) {
-                        contador++;
-                    }
-                    if (campo[x][y-1]==-1) {
-                        contador++;
-                    }
-                    if(campo[x-1][y-1]==-1) {
-                        contador++;
-                    }
+                if (campo[x+1][y]==-1 && x < 4) {
+                    contador++;
+                }
+                if (campo[x+1][y+1]==-1) {
+                    contador++;
+                }
+                if (campo[x][y-1]==-1) {
+                    contador++;
+                }
+                if (campo[x][y+1]==-1) {
+                    contador++;
+                }
+                if (campo[x+1][y]==-1 && x < 4) {
+                    contador++;
+                }
+                if (campo[x+1][y+1]==-1) {
+                    contador++;
+                }
+                if (campo[x][y-1]==-1) {
+                    contador++;
+                }
+                if (campo[x-1][y]==-1) {
+                    contador++;
+                }
+                if (campo[x-1][y-1]==-1) {
+                    contador++;
+                }
+                if (campo[x+1][y-1]==-1) {
+                    contador++;
+                }
+                if (campo[x-1][y+1]==-1) {
+                    contador++;
                 }
             }
-            campo[x][y] = contador;
+                // else {
+                //     if(campo[x-1][y]==-1) {
+                //         contador++;
+                //     }
+                //     if (campo[x][y-1]==-1) {
+                //         contador++;
+                //     }
+                //     if(campo[x-1][y-1]==-1) {
+                //         contador++;
+                //     }
+                // }
+                 campo[x][y] = contador;
+            }
+            
         }
     }
 
     for (int i = 0; i < 5; i++) {
         for (int j = 0; j < 5; j++) {
-            printf("%d ", campo[i][j]);
+            printf("[%d]", campo[i][j]);
         }
         printf("\n");
     }
