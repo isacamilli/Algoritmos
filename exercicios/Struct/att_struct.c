079bcb498e03757aa9971ac4e273a74ae0a5de7a
isacamilli <165300352+isacamilli@users.noreply.github.com>
2024-12-10 17:07:54 +0000

    att

--- exercicios/Struct/att_struct.c
+++ exercicios/Struct/att_struct.c
@@ -14,9 +14,32 @@ struct Paciente* criar_paciente(struct Paciente fila[], int tot_fila){
     printf("Insira o nome do paciente: ");
     scanf("%s", fila[tot_fila].nome);
     printf("Insira a idade do paciente: ");
-    scanf("%s", &fila[tot_fila].idade);
+    scanf("%d", &fila[tot_fila].idade);
     printf("Insira o tipo do atendimento: ");
     scanf("%s", fila[tot_fila].idade);
+    printf("O paciente é preferencial? 1 - Sim, 0 - Não");
+    scanf("%d", &fila[tot_fila].pref);
+    if(fila[tot_fila].pref == 1){
+        printf("Qual é o tipo de preferência: 1 - Idoso, ");
+        printf("2 - Gestante/Lactante, ");
+        printf("3 - Deficiência");
+        scanf("%d", &fila[tot_fila].pref);
+        switch (fila[tot_fila].pref)
+        {
+        case 1:
+            if(fila[0].pref != 1){
+                fila[tot_fila].id = 1;
+                for(int i = 0;i < tot_fila; i++){
+                    
+                }
+            }
+
+            break;
+        
+        default:
+            break;
+        }
+    }
 };
 
 
