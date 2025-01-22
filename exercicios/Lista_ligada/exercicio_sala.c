95c28796efb0ce1962c4b5f90154b8d8ce9b188f
Isabella Camilli Meletani <20241014040028@ifrn.local>
2025-01-22 11:59:58 -0300

    att atividade

---
+++ exercicios/Lista_ligada/exercicio_sala.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Lista {
+    struct Lista *proximo;
+    int nome_paciente;
+    int tipo_paciemte
+};
+
+struct Lista *inicio = NULL;
+
+void adicionar_paciente_normal(int nome_paciente){
+
+}
