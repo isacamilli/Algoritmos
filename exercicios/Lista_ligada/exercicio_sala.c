5e997db2eb9cd6bcd9af0e760e6769cc3dca4ad6
isacamilli <165300352+isacamilli@users.noreply.github.com>
2025-01-23 11:23:56 +0000

    att

--- exercicios/Lista_ligada/exercicio_sala.c
+++ exercicios/Lista_ligada/exercicio_sala.c
@@ -13,21 +13,65 @@ struct Lista *inicio = NULL;
 void adicionar_paciente(int tipo_paciente, char* nome_paciente){
     struct Lista *novo_paciente = (struct Lista *)malloc(sizeof(struct Lista));
     
-    if (inicio == NULL){
         novo_paciente->tipo_paciente = tipo_paciente;
         strcpy(novo_paciente->nome_paciente, nome_paciente);
         novo_paciente->proximo = inicio;
         inicio = novo_paciente;
-    }
-    else if (tipo_paciente == 0){
-        struct Lista *atual = 
-        while (/* condition */)
-        {
-            /* code */
-        }
-        
-    }
     
+}
+
+int remover_paciente(char* nome_paciente){
+    struct Lista *atual = inicio;
+    struct Lista *anterior = NULL;
+    int paciente_encontrado = 0;
+
+    while (atual != NULL) {
+        if (atual->nome_paciente == nome_paciente){ // se achar o valor
+            if (anterior == NULL){ 
+                struct Lista *temp = inicio;
+                inicio = inicio->proximo;
+                free(temp); 
+
+            } else{ // se nao for NULL
+                anterior->proximo = atual->proximo; // o proximo do anterior é o proximo do atual
+                free(atual);
+        }
+        printf("Paciente");
+    }
+
+    else{
+        paciente_encontrado = 1;
+    }
+
+    return paciente_encontrado;
+    
+}
+
+}
+
+int atender_paciente(int preferenciais_atendidos){
+
+    struct Lista *atual = inicio;
+    struct Lista *anterior = NULL;
+    int atendimento = 0;
+
+    if (preferenciais_atendidos < 2){
+        while (atual != NULL){
+            if (atual->tipo_paciente == 0){
+                printf("Paciente %s está sendo atendido.\n", atual->nome_paciente);
+                remover_paciente(atual->nome_paciente);
+                atendimento++;
+                break;
+            }
+        }
+        if (atendimento != 0){
+            preferenciais_atendidos++;
+        }
+        else{
+            
+        }
+    }
+    return preferenciais_atendidos;
 }
 
 int imprimir_menu()
@@ -52,6 +96,7 @@ int imprimir_menu()
 // int main(){
 
 //     int nome_paciente, tipo_paciente;
+//     int preferenciais_atendidos = 0
 //     printf("Nome do paciente: ");
 //     scanf("%d", &nome_paciente);
 
